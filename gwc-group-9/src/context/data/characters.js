005bf934e3efbd397c28c484be79294d52aa8e2d
simone-c18 <151193305+simone-c18@users.noreply.github.com>
2025-10-02 18:09:47 -0400

    created basic JS files for storing character information

---
+++ gwc-group-9/src/context/data/characters.js
@@ -0,0 +1,57 @@
+//character data structure
+
+export const CHARACTERS = [
+
+    //java
+    {
+        id: 'java',
+        name: 'Java',
+        bio: "cute sentence that describes java character",
+
+        //paths to visual assets
+        avatarPath: '..', //add path
+        spritePath: '..',
+
+        //link to story and quiz ("date")
+        storyId: 'story-java',
+        quizId: 'quiz-java',
+    },
+
+    //c
+    {
+        id: 'c',
+        name: 'C',
+        bio: "cute sentence that describes java character",
+
+        //paths to visual assets
+        avatarPath: '..', //add path
+        spritePath: '..',
+
+        //link to story and quiz ("date")
+        storyId: 'story-c',
+        quizId: 'quiz-c',
+    },
+
+    //python
+    {
+        id: 'python',
+        name: 'Python',
+        bio: "cute sentence that describes java character",
+
+        //paths to visual assets
+        avatarPath: '..', //add path
+        spritePath: '..',
+
+        //link to story and quiz ("date")
+        storyId: 'story-python',
+        quizId: 'quiz-python',
+    },
+
+
+];
+
+//function to find character data by id
+export const getCharacterById = (id) => {
+    return CHARACTERS.find(character => character.id === id);
+};
+
