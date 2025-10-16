38e5cd5d3ce82fe7126b5e54ed751404d57820b8
Abbby1007 <Abigailloken25@gmail.com>
2025-10-16 12:20:28 -0400

    updated Landing Page and Lesson Page

--- lessonPage.js
+++ lessonPage.js
@@ -1,14 +1,23 @@
 import './lessonPage.css'
 function lessonPage(){
     return (
- <div id="text_Box">   
-            <h1 id="text_Title"> Title </h1> 
+ <div> 
+   
+    <div id="text_Box">   
+            <h1 id="text_Title"> Variables </h1> 
 
              <p id="text"> <b> Data types </b><br>
-            </br> Therea re numerous basic data types used when creating variables; <br>
-          </br> int, char, double, and float </p> 
-           <button>Go On Date</button> 
-       
+            </br> There are numerous basic data types used when creating variables <br>
+          </br> int, char, double, and float <br></br>
+          <b> Integers</b> <br></br>
+          An integer represents a whole-number value. For example, "7" or "12." <br></br>
+          To declare an integer variable in C, the following syntax is used: <br></br>
+          int val = 3; <br></br>
+          The keyword 'int' is used followed by the variable name.
+           </p>
+<p> </p>
         </div>
+    <button> Go On Date </button>
+    </div>
     )
 }
