a55ecc996a2eb1b1249a85ad399cb7ea2295b673
Abbby1007 <Abigailloken25@gmail.com>
2025-10-20 17:25:38 -0400

    add userPickResponse page and css

---
+++ gwc-group-9/app/userPickResponse.tsx
@@ -0,0 +1,15 @@
+import '../src/styles/userPickResponse.css';
+export default function pick(){
+    return (
+<div className="App">
+  <button> BACK </button>
+
+  <div className="response">
+    <h3> Choose your Response: </h3>
+     </div>
+   <div className="npcConversation">  </div>
+</div>
+
+
+    );
+}
