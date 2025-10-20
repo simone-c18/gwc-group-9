9f874026efd42106f586a67fecadbacd91bef49b
Abbby1007 <Abigailloken25@gmail.com>
2025-10-20 11:04:34 -0400

    Add lesson page and css
    This is the lesson page css and tsx file

---
+++ gwc-group-9/app/lesson.tsx
@@ -0,0 +1,15 @@
+import '../src/styles/lesson.css';
+
+export default function lesson(){
+    return(
+<div className="App">
+<div className="box"> 
+  <h1 id="text_Title"> Variables </h1>
+
+  <p> Information </p>
+  </div>  
+
+<button> Go On Date</button>
+</div>
+    );
+}
